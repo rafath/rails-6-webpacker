1bdb26f21ba7bb26716711b4b091bb37ce884240
Ross Kaffenberger <rosskaff@gmail.com>
2021-02-17 20:06:55 -0500

    Remove custom css webpack loader rules
    Webpacker 6 will provide loader rules by default when css webpack
    dependencies are detected. The custom loader rules were not set up
    correctly to work with the rest of the webpack config.
    
    Fixes @rails/webpacker issue #2916

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -6,20 +6,4 @@ module.exports = {
       ReactDOM: 'react-dom',
     }
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["css-loader", "postcss-loader", { loader: "sass-loader", options: { implementation: require("sass") } },
-        ],
-      },
-    ],
-  }
 }
